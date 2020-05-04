68f02f9353c0dceb6c16ac06bd659ef7b10ac40d
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-04 22:39:21 +0000

    Dummy data populated

--- Captain_console/home/views.py
+++ Captain_console/home/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+context = [
+    {'name': 'supermario','price': 123},
+    {'name': 'crashbandicoot',"price": 50}]
+
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context={"data": context})
