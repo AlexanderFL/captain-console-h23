02638273d44063d23f8e159bb2422199ac7f57a1
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-05 22:56:00 +0000

    urls for games and consoles

--- Captain_console/store/urls.py
+++ Captain_console/store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="store_index")
+    path('', views.index, name="store_index"),
+    path('games', views.index, name="games_index")
+    path('consoles', views.index, name="consoles_index")
 ]
