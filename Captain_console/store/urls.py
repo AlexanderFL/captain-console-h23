7662f51d7e49763c28610e381e6989bd101c6d54
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-05 23:08:00 +0000

    nav bar

--- Captain_console/store/urls.py
+++ Captain_console/store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="store_index"),
-    path('games', views.index, name="games_index")
+    path('games', views.index, name="games_index"),
     path('consoles', views.index, name="consoles_index")
 ]
