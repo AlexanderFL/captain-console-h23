b91dd9ac57c25d4de0a9cdeae8ef4ed6c17f5bce
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-06 21:32:39 +0000

    url i checkout

--- Captain_console/checkout/urls.py
+++ Captain_console/checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="checkout_index")
+    path('', views.index, name="checkout_index"),
+    path('/contactinfo', views.contactinfo, name="contactinfo_index")
 ]
