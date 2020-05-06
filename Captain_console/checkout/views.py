8f4e90bc8f7f3a6a188d2362d9aee21bca5d84c1
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-06 21:41:30 +0000

    checkout fixed

--- Captain_console/checkout/views.py
+++ Captain_console/checkout/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'checkout/index.html')
+
+def contactinfo(request):
+    return render(request, 'checkout/contactinfo.html')
