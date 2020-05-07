eccb85adbb2ab7aa22547c90ff8a3986367400b0
helgaarna <helgaa19@ru.is>
2020-05-07 13:20:32 +0000

    checkout

--- Captain_console/checkout/views.py
+++ Captain_console/checkout/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'checkout/index.html')
 
 def contactinfo(request):
     return render(request, 'checkout/contactinfo.html')
+
+def shipping(request):
+    return render(request, 'checkout/shipping.html')
+
+def payment(request):
+    return render(request, 'checkout/payment.html/')
