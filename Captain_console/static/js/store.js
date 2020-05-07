26e288fccca362f217d23e9fea0dedf6c6d302db
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-07 19:18:09 +0000

    store layouiting

--- Captain_console/static/js/store.js
+++ Captain_console/static/js/store.js
@@ -1,6 +1,17 @@
 
 //Allow dropdown in store
+var store_dropdown = document.getElementsByClassName(store_dropdown);
+
+store_dropdown.onclick = function(event) {
+
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
+
+    instance.open()
   });
+
+$('.dropdown-trigger').dropdown();
