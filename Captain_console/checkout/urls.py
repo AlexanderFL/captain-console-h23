8b518555ceba5fd20e8c7b403be2431054983fa8
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-07 21:25:32 +0000

    confirmation view path connected

--- Captain_console/checkout/urls.py
+++ Captain_console/checkout/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="checkout_index"),
+    path('', views.index, name="checkout_index"), #/checkout
     path('contactinfo', views.contactinfo, name="contactinfo_index"),
     path('contactinfo/shipping', views.shipping, name="shipping_index"),
-    path('contactinfo/shipping/payment', views.payment, name="payment_index")
+    path('contactinfo/shipping/payment', views.payment, name="payment_index"),
+    path('contactinfo/shipping/payment/confirmation', views.confirmation, name="confirmation_index"),
 ]
--- Captain_console/static/js/store.js
+++ Captain_console/static/js/store.js
@@ -2,8 +2,3 @@ document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('select');
             var instances = M.FormSelect.init(elems);
         });
-
-$(document).ready(function() {
-    // Select - Single
-    $('select:not([multiple])').material_select();
-});
