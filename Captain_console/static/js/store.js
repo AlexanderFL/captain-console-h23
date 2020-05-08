22e169c7d3ec5f569b346d0e2fa29b798366e741
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-08 17:45:41 +0000

    js

--- Captain_console/static/js/store.js
+++ Captain_console/static/js/store.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
             var instances = M.FormSelect.init(elems);
         });
 
-$(document).ready(function(){
-    $('.orderby').change(function(){
-        alert('Change detected')
+window.onload = function(){
+    $(document).ready(function(){
+        $('#orderby').on('change', function(){
+            alert('Change detected');
+        });
     });
-});
+}
 
 /*
 var orderby = document.getElementById("orderby")
@@ -16,4 +18,7 @@ orderby.addEventListener('change', filter_by_genre);
 
 function filter_by_genre() {
     console.log(orderby.innerText)
+    console.log(orderby.options.length)
+    console.log(orderby.selectedIndex)
+    console.log()
 }*/
