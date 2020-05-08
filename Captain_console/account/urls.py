3a1ddf81a6c5d2423dae8ea9cd48bcb5b28e7540
heidars19 <heidars19@ru.is>
2020-05-08 17:45:20 +0000

    Profile css and edit profile, button links

--- Captain_console/account/urls.py
+++ Captain_console/account/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:id>', views.index, name="account_index")
+    path('<int:id>', views.index, name="account_index"),
+    path('change_profile/<int:id>', views.edit, name="change_profile")
 ]
+
