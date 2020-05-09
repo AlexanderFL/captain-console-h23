ea5f3a54fb9f51f2dab75ade06a76db38edda2ca
Heidars19 <heidar.sigurjons@gmail.com>
2020-05-09 20:28:18 +0000

    checkout look setup, synchronized

--- Captain_console/checkout/views.py
+++ Captain_console/checkout/views.py
@@ -3,16 +3,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'checkout/index.html')
+    return render(request, 'checkout/index.html', context={'page_checkout': 'contactinfo'})
 
 def contactinfo(request):
-    return render(request, 'checkout/contactinfo.html')
+    return render(request, 'checkout/index.html', context={'page_checkout': 'contactinfo'})
 
 def shipping(request):
-    return render(request, 'checkout/shipping.html')
+    return render(request, 'checkout/index.html', context={'page_checkout': 'shipping'})
 
 def payment(request):
-    return render(request, 'checkout/paymentinfo.html/')
+    return render(request, 'checkout/index.html', context={'page_checkout': 'paymentinfo'})
 
 def confirmation(request):
-    return render(request, 'checkout/confirmation.html')
+    return render(request, 'checkout/index.html', context={'page_checkout': 'confirmation'})
+
+#
+# # Create your views here.
+# def index(request):
+#     return render(request, 'checkout/index.html', context={'page-checkout'=})
+#
+# def contactinfo(request):
+#     return render(request, 'checkout/contactinfo.html')
+#
+# def shipping(request):
+#     return render(request, 'checkout/shipping.html')
+#
+# def payment(request):
+#     return render(request, 'checkout/paymentinfo.html/')
+#
+# def confirmation(request):
+#     return render(request, 'checkout/confirmation.html')
