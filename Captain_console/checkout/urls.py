c8929b0d5b28d172a59427fb761a8b26c6c9eebf
Heidars19 <heidar.sigurjons@gmail.com>
2020-05-09 01:22:33 +0000

    fixed links in checkout, can go back and forth

--- Captain_console/checkout/urls.py
+++ Captain_console/checkout/urls.py
@@ -4,7 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.index, name="checkout_index"), #/checkout
     path('contactinfo', views.contactinfo, name="contactinfo_index"),
-    path('contactinfo/shipping', views.shipping, name="shipping_index"),
-    path('contactinfo/shipping/payment', views.payment, name="payment_index"),
-    path('contactinfo/shipping/payment/confirmation', views.confirmation, name="confirmation_index"),
+    path('shipping', views.shipping, name="shipping_index"),
+    path('payment', views.payment, name="payment_index"),
+    path('confirmation', views.confirmation, name="confirmation_index"),
 ]
+
+# urlpatterns = [
+#     path('', views.index, name="checkout_index"), #/checkout
+#     path('contactinfo', views.contactinfo, name="contactinfo_index"),
+#     path('contactinfo/shipping', views.shipping, name="shipping_index"),
+#     path('contactinfo/shipping/payment', views.payment, name="payment_index"),
+#     path('contactinfo/shipping/payment/confirmation', views.confirmation, name="confirmation_index"),
+# ]
--- Captain_console/checkout/views.py
+++ Captain_console/checkout/views.py
@@ -12,7 +12,7 @@ def shipping(request):
     return render(request, 'checkout/shipping.html')
 
 def payment(request):
-    return render(request, 'checkout/payment.html/')
+    return render(request, 'checkout/paymentinfo.html/')
 
 def confirmation(request):
     return render(request, 'checkout/confirmation.html')
