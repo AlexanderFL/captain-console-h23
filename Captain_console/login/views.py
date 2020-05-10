b06543c347d5623bd676e0a89075fc274803e7ad
AlexanderFL <alex98freyr@gmail.com>
2020-05-10 19:44:31 +0000

    Working on registration
    +Added register.js for the logic

--- Captain_console/login/views.py
+++ Captain_console/login/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
 def index(request):
     return render(request, 'login/index.html', context={'page_login': 'login_index'})
 
+
+@csrf_exempt
 def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        print(username)
+        print(email)
+        print(password)
+
     return render(request, 'login/index.html', context={'page_login': 'login_register'})
