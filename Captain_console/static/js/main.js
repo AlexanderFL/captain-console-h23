939cefec3cddb8ec79f3003c58c92d22b59132c2
AlexanderFL <alex98freyr@gmail.com>
2020-05-10 12:00:06 +0000

    Few fixes
    +Fixed: Mobile navbar could not search in store
    
    +Changed: Removed unnecessary code
    
    +Changed: Made categories in product details bold

--- Captain_console/static/js/main.js
+++ Captain_console/static/js/main.js
@@ -11,14 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Parallax.init(elems, '');
 });
 
+function locationReplace(val){
+    window.location.replace('http://localhost:8000/store/search/' + val)
+}
+
 /* These functions are kind whack, but they work*/
 $('#search-bar-submit').on('click', function(){
-    window.location.replace('http://localhost:8000/store/search/' + $('#search-bar')[0].value)
+    locationReplace($('#search-bar')[0].value)
 });
 
 $('#search-bar').on('keyup', function(event){
     // If keypress is 'Enter'
     if(event.keyCode === 13){
-        window.location.replace('http://localhost:8000/store/search/' + $('#search-bar')[0].value)
+        locationReplace($('#search-bar')[0].value)
+    }
+});
+
+$('#mobile-search-bar').on('keyup', function(event){
+    if(event.keyCode === 13){
+        locationReplace($('#mobile-search-bar')[0].value)
     }
 });
