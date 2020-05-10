b856fc45f90fa8be98cb3fabc7dada096db5f030
Heidars19 <heidar.sigurjons@gmail.com>
2020-05-10 08:52:15 +0000

    Dynamic links for login

--- Captain_console/login/views.py
+++ Captain_console/login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'login/index.html')
+    return render(request, 'login/index.html', context={'page_login': 'login_index'})
 
 def register(request):
-    return render(request, 'login/index2.html')
+    return render(request, 'login/index.html', context={'page_login': 'login_register'})
