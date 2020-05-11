c3631b64f25adb321e44f7d52667056d1dffc1ae
AlexanderFL <alex98freyr@gmail.com>
2020-05-11 15:05:25 +0000

    Sign in hopefully complete

--- Captain_console/static/js/login.js
+++ Captain_console/static/js/login.js
@@ -1,18 +1,23 @@
 
 window.onload = function(){
+    $("#loader").hide()
     $('#sign-in-button').on('click', function(){
         let email = $("#id_username").val() //TODO: Change to email
         let password = $("#password").val()
 
         M.Toast.dismissAll();
+        // If email is empty or contains only spaces
         if(email.trim().length === 0){
             M.toast({html: "Please enter your email", classes: "red"})
             return
         }
+        // If password is empty or contains only spaces
         if(password.trim().length === 0){
             M.toast({html: "Please enter your password", classes: "red"})
             return
         }
+
+        $("#loader").show()
 
         $.ajax({
             url: "",
@@ -22,6 +27,7 @@ window.onload = function(){
                 password: password
             },
             success : function(response){
+                $("#loader").hide()
                 if (response.status === 0){
                     M.toast({html: response.message, classes: "red"})
                 } else if(response.status === 1){
