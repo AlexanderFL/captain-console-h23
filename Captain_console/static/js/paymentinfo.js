4aa82b5877a80bb845d2415b75b027776f1195d3
AlexanderFL <alex98freyr@gmail.com>
2020-05-12 01:32:44 +0000

    Working on checkout
    +Changed: Made the checkout.css look much better
    
    Ongoing: Working on paymentinfo.js

--- Captain_console/static/js/paymentinfo.js
+++ Captain_console/static/js/paymentinfo.js
@@ -1,4 +1,24 @@
 
 window.onload = function(){
     $('.modal').modal();
+
+    function makeOrder(){
+        let cardHolder = $('#card_nameholder').val()
+        let cardNumber = $('#card_number').val()
+        let expireDate = $('#exp_date').val()
+        let cvc = $('#cvc_number').val()
+
+        // TODO: Verify all card details are correct
+
+        // Check if user checked remember card details
+        if ($('#remember-creditcard').prop('checked')){
+            //Save card into database
+        }
+
+
+    }
+
+    $('#confirm-payment').on('click', function(){
+       makeOrder()
+    });
 }
