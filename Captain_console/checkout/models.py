b14017b6bca571c1542acf9f1776fc8cf19ce2d3
heidars19 <heidars19@ru.is>
2020-05-12 17:29:46 +0000

    checkout.models.py init, removed from store.models.py

--- Captain_console/checkout/models.py
+++ Captain_console/checkout/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from account.models import User
 
 
-# Create your models here.
+# Maybe these models can change locations but I'll leave it here for now
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.FloatField()
+    tracking_nr = models.CharField(max_length=128)
+    date = models.DateField(auto_now_add=True)
+
+
+class OrderProduct(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+#######################
--- Captain_console/store/models.py
+++ Captain_console/store/models.py
@@ -89,18 +89,4 @@ class Review(models.Model):
         return str(self.rating)
 
 
-# Maybe these models can change locations but I'll leave it here for now
-class Order(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_price = models.FloatField()
-    tracking_nr = models.CharField(max_length=128)
-    date = models.DateField(auto_now_add=True)
 
-
-class OrderProduct(models.Model):
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    price = models.FloatField()
-
-#######################
