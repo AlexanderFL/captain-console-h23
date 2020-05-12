e19c2a842d9d2adf7b17bb09879e575fdf4b6bf3
heidars19 <heidars19@ru.is>
2020-05-12 17:36:20 +0000

    fixed checkout.models, moved orderid from orderproduct to orderproductid in order

--- Captain_console/checkout/models.py
+++ Captain_console/checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from account.models import User
+from store.models import Product
 
 
 # Maybe these models can change locations but I'll leave it here for now
@@ -8,10 +9,9 @@ class Order(models.Model):
     total_price = models.FloatField()
     tracking_nr = models.CharField(max_length=128)
     date = models.DateField(auto_now_add=True)
-
+    orderproduct_id = models.ForeignKey(OrderProduct, on_delete=models.CASCADE)
 
 class OrderProduct(models.Model):
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.FloatField()
