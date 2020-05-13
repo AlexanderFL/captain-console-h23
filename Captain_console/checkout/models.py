eea77181441694b1345ede8f53a5952492ff99eb
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-13 16:01:01 +0000

    f

--- Captain_console/checkout/models.py
+++ Captain_console/checkout/models.py
@@ -4,6 +4,7 @@ from store.models import Product
 
 
 class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.FloatField()
     tracking_nr = models.CharField(max_length=128)
     date = models.DateField(auto_now_add=True)
@@ -11,15 +12,26 @@ class Order(models.Model):
 
 
 class OrderProduct(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.FloatField()
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
 
-
     # Creates new order product in DB
     def add_product_to_cart(self, product_id, quantity, user_id=None):
+
+        orders = Order.objects.get(pk=user_id)
+
+        for order in orders:
+            status = order.confirmed
+
+            if status == False:
+                shopping_cart = order
+
+            else:
+                Order.objects.create()
+
+
         product = Product.objects.get(pk=product_id)
         user = User.objects.get(pk=user_id)
         total_price = product.price * quantity * (100 - product.discount) / 100
@@ -31,5 +43,4 @@ class OrderProduct(models.Model):
         order_product = Product.objects.get(pk=prod_id)
         print(order_product.id)
         new_quantity = order_product.quantity +1
-
         order_product.update(quantity=new_quantity)
