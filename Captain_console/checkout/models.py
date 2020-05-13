0b8dbdd648e3b684102a9b0153a43a698d08059b
Eyþór Óli Borgþórsson <eythor.borgthorsson@marel.com>
2020-05-13 21:59:47 +0000

    checkout edit, but responsiveness is not good

--- Captain_console/checkout/models.py
+++ Captain_console/checkout/models.py
@@ -18,8 +18,11 @@ class OrderProduct(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
 
+    '''
+    Checks if user has a non-confirmed order ("shopping cart")
+    '''
+
     def check_active_order(self, user_id):
-        # Check if there is a non-confirmed order for this user
         empty_order = False
         orders = Order.objects.filter(pk=user_id)
         for order in orders:
@@ -37,6 +40,7 @@ class OrderProduct(models.Model):
     '''
     Price calculated
     '''
+
     def calculate_price(self, quantity, product):
         total_price = product.price * quantity * (100 - product.discount) / 100
         print("Total price is:" + str(total_price))
@@ -45,6 +49,7 @@ class OrderProduct(models.Model):
     '''
     Checks if product is already in cart
     '''
+
     def check_if_already_in_cart(self, user_id, product):
         order_products = OrderProduct.objects.filter(user_id=user_id)
         for order_product in order_products:
@@ -55,6 +60,7 @@ class OrderProduct(models.Model):
     '''
     Creates a new order_product in cart
     '''
+
     def create_product_in_cart(self, product, quantity, user, shopping_cart):
         total_price = self.calculate_price(quantity, product)
         OrderProduct.objects.create(product_id=product, quantity=quantity, price=total_price, user_id=user,
@@ -64,6 +70,7 @@ class OrderProduct(models.Model):
     '''
     Updates product in cart
     '''
+
     def update_product_in_cart(self, order_product, quantity, user, shopping_cart):
         new_quantity = order_product.quantity + quantity
         total_price = self.calculate_price(new_quantity, order_product.product_id)
@@ -74,6 +81,7 @@ class OrderProduct(models.Model):
     '''
     Adds product to cart or updates quantity if already in cart
     '''
+
     def add_product_to_cart(self, product_id, quantity, user_id):
         user = User.objects.get(pk=user_id)
         product = Product.objects.get(pk=product_id)
@@ -83,20 +91,16 @@ class OrderProduct(models.Model):
 
         # Check if item is in cart
         in_cart = self.check_if_already_in_cart(user_id, product)
-        print(product.name)
-        print(product.id)
-        print(in_cart)
 
-
-        if in_cart != False:
-            self.update_product_in_cart(in_cart, quantity, user, shopping_cart)
-        else:
+        if not in_cart:
             self.create_product_in_cart(product, quantity, user, shopping_cart)
+        else:
+            self.update_product_in_cart(in_cart, quantity, user, shopping_cart)
 
-    # Increase quantity of item in cart
-    def add_item(self, prod_id):
-        print(prod_id)
-        order_product = Product.objects.get(pk=prod_id)
-        print(order_product.id)
-        new_quantity = order_product.quantity + 1
-        order_product.update(quantity=new_quantity)
+    # # Increase quantity of item in cart
+    # def add_item(self, prod_id):
+    #     print(prod_id)
+    #     order_product = Product.objects.get(pk=prod_id)
+    #     print(order_product.id)
+    #     new_quantity = order_product.quantity + 1
+    #     order_product.update(quantity=new_quantity)
