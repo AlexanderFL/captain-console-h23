9cf41721985281ec9df1035c5df7a2c779820606
Heiðar Sigurjónsson <heidar@fss.is>
2020-05-14 10:32:31 +0000

    terms and about us

--- Captain_console/home/urls.py
+++ Captain_console/home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home_index"),
-    path('logout', views.logout, name="home_index_logout")
+    path('terms', views.terms, name="home_terms"),
+    path('aboutus', views.aboutus, name="home_aboutus"),
+    path('logout', views.logout, name="home_index_logout"),
 ]
