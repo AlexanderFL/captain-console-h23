70e5ffe290463eefa4b3a8b3df7f8cd508c5d3b7
Heiðar Sigurjónsson <heidar@fss.is>
2020-05-15 12:04:53 +0000

    news widget on frontpage init, needs working links

--- Captain_console/home/views.py
+++ Captain_console/home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from store.models import Product
+from home.models import News
 
 
 def index(request):
-    context = {'products': Product.objects.all()[:3], 'user_session': request.session.get('user_session')}
+    context = {'products': Product.objects.all()[:3], 'articles': News.objects.all().order_by('date')[:3], 'user_session': request.session.get('user_session')}
     return render(request, 'home/index.html', context)
 
 
