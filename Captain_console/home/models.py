53177fa7daa758f057741e46e41b9044db7f7479
Heiðar Sigurjónsson <heidar@fss.is>
2020-05-15 10:56:33 +0000

    news model

--- Captain_console/home/models.py
+++ Captain_console/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=64)
+    date = models.DateField()
+    short = models.CharField(max_length=256)
+    article = models.TextField(blank=True)
