083096961025e6628f305ce304a16b7179f703ee
AlexanderFL <alex98freyr@gmail.com>
2020-05-15 13:12:42 +0000

    New password page, no functionality yet

--- Captain_console/home/views.py
+++ Captain_console/home/views.py
@@ -4,24 +4,24 @@ from home.models import News
 
 
 def index(request):
-    context = {'products': Product.objects.all()[:3], 'articles': News.objects.all().order_by('-date')[:3], 'user_session': request.session.get('user_session')}
+    context = {'products': Product.objects.all()[:3], 'articles': News.objects.all().order_by('-date')[:3]}
     return render(request, 'home/index.html', context)
 
 
 def terms(request):
-    context = {'products': Product.objects.all()[:3], 'user_session': request.session.get('user_session')}
+    context = {'products': Product.objects.all()[:3]}
     return render(request, 'home/terms.html', context)
 
 
 def aboutus(request):
-    context = {'products': Product.objects.all()[:3], 'user_session': request.session.get('user_session')}
+    context = {'products': Product.objects.all()[:3]}
     return render(request, 'home/aboutus.html', context)
 
 
 def logout(request):
     if request.session.__contains__('user_id'):
         request.session.pop('user_id')
-    context = {'products': Product.objects.all()[:3], 'user_session': request.session.get('user_session')}
+    context = {'products': Product.objects.all()[:3]}
     return render(request, 'home/index.html', context)
 
 
